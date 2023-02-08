a39bc15426b2111bbac6b4586cce929895b341c6
lennynsprado <lennyn_michaelprado@hotmail.com>
2023-02-07 19:02:47 -0500

    Changed message to be printed

--- Test/ContentView.swift
+++ Test/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
-            Text("Hello James")
+            Text("Hello this is for the second commit!")
         }
         .padding()
     }
